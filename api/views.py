08e7464e40844b3206087d4c53019c93911ae511
crinaioana29 <crinadionisie@gmail.com>
2023-11-01 23:37:37 +0200

    Added dataframe

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .web_crawler import main
@@ -7,6 +9,6 @@ from rest_framework import status
 @api_view(['GET'])
 def product_list(request, word=None, format=None):
     if request.method == 'GET':
-        main.web_scrapping(word)
-        return Response(status=status.HTTP_200_OK)
+        df = main.web_scrapping(word)
+        return Response(df.to_json(orient='records', force_ascii=False, default_handler=str, lines=True).encode('utf-8').decode('unicode-escape'), status=status.HTTP_200_OK)
     return Response(status=status.HTTP_404_NOT_FOUND)
--- api/web_crawler/main.py
+++ api/web_crawler/main.py
@@ -1,7 +1,8 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from .request_model import RequestModel
-
+from unidecode import unidecode
 
 def web_scrapping(word):
     try:
@@ -14,16 +15,23 @@ def web_scrapping(word):
         results = soup.find(id="list_cart_holder")
 
         listing_data = results.find_all('a', class_="main_items")
+        df = scrape_data_to_dataframe(listing_data)
 
-        for l_a in listing_data:
-            title = l_a.find("span", class_="title")
-            price = l_a.find("span", class_="price shadow")
-
-            if title is not None and price is not None:
-                print(title.text, price.text[4:])
-            else:
-                print("Incomplete data for a listing")
     except requests.exceptions.RequestException as e:
         print(f"Request Exception: {e}")
     except Exception as e:
         print(f"An error occurred: {e}")
+    return df
+
+def scrape_data_to_dataframe(listing_data):
+    data_list = []
+    data = {}
+    for l_a in listing_data:
+        title = l_a.find("span", class_="title")
+        price = l_a.find("span", class_="price shadow")
+        if title is not None and price is not None:
+            data = {"Title": unidecode(title.text.strip()),
+                    "Price": unidecode(price.text[4:].strip())}
+
+        data_list.append(data)
+    return data_list
